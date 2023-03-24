87e02ec93048a2a00adfe06ef1120a6787cb6493
Denzoll <elikhanovden@gmail.com>
2023-03-24 17:03:37 +0300

    oh shit were we go again

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import "./scss/app.scss";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
+import pizzas from "./assets/pizzas.json";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {pizzas.map((obj) => (
+              <PizzaBlock {...obj} key={obj.id}
+                // title={obj.title}
+                // price={obj.price + "₽"}
+                // size={obj.sizes}
+                // imageUrl={obj.imageUrl}
+                // sizes={obj.sizes}
+                // types={obj.types}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
