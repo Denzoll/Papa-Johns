9eaeb01245eb3f67659016591e1a838f8212da4a
Denzoll <elikhanovden@gmail.com>
2023-03-30 18:01:47 +0300

    end Routes

--- src/App.js
+++ src/App.js
@@ -2,55 +2,26 @@ import React from "react";
 
 import "./scss/app.scss";
 import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
-import Skeleton from "./components/PizzaBlock/skeleton";
+import { Home } from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import { NotFound } from "./pages/NotFound";
+import { Cart } from "./pages/Cart";
+
 // import Skeleton from "./components/PizzaBlock/skeleton";
 
-
 function App() {
-
-  const [items, setItems] = React.useState([])
-  const [isLoading, setIsLoading] = React.useState(true)
-
-  React.useEffect(() => {
-
-
-    fetch('https://6421879286992901b2b5ca52.mockapi.io/items').then((res) => {
-      return res.json();
-    }).then((arr) => {
-      setItems(arr)
-      setIsLoading(false)
-    })
-  }, [])
   return (
     <div className="wrapper">
       <Header />
 
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-          {
-            isLoading 
-            ? [...new Array (8)].map((_, index) => <Skeleton  key={index} />) :
-            items.map((obj) => (
-              <PizzaBlock {...obj} key={obj.id}
-                // title={obj.title}
-                // price={obj.price + "₽"}
-                // size={obj.sizes}
-                // imageUrl={obj.imageUrl}
-                // sizes={obj.sizes}
-                // types={obj.types}
-              />
-            ))
-          }
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+
+            <Route path="/cart.html" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom'
 import logoSvg from '../assets/img/pizza-logo.svg'
 
 const Header = () => {
     return (
     <div className="header">
             <div className="container">
+              <Link to='/' >
               <div className="header__logo">
                 <img width="38" src={logoSvg} alt="Pizza logo" />
                 <div>
@@ -11,6 +13,7 @@ const Header = () => {
                   <p>самая вкусная пицца во вселенной</p>
                 </div>
               </div>
+              </Link>
               <div className="header__cart">
                 <a href="/cart.html" className="button button--cart">
                   <span>520 ₽</span>
